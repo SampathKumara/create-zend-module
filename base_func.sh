9361881a035e8ae2cddd48e56217d3cf8a790f1e
Sampath <sampath.skyalliances@gmail.com>
2019-04-26 17:03:36 +0530

    current directory check added to zend_module.sh

--- base_func.sh
+++ base_func.sh
@@ -1,7 +1,15 @@
+#title           :base_func.sh
+#description     :This script contains helper functions.
+#author		 :Sampath K Abeysinghe
+#date            :25-Apr-2019
+#version         :0.1
+#usage		 :
+#notes           :Not executable
+#===================================================================
+
 function exit_func {
   	echo "Exitting...[0m"
-	if [ $# -eq 1 ] && [ $1 -eq 1 ]
-	then
+	if [ $# -eq 1 ] && [ $1 -eq 1 ]; then
 		exit $1
 	fi
 	exit 0
@@ -19,12 +27,36 @@ function show_message {
 	echo  "[0m"
 }
 
-function create_dir(){
-  cd $1
-  if [ -d  $2 ]
-  then
-    show_error "Directoroy $2 already exists in the distination path $1"
-  fi
-  mkdir $2
-  show_message "Directory $2 created in $1"
+function create_dir {
+	cd $1
+  	if [ $# -lt 2 ]; then
+		show_error "Plese provide the base directory and at-lest one new directory name"
+	elif [ -d  $2 ]; then
+		show_error "Directoroy $2 already exists in the distination path $1"
+	fi
+	count=0
+	for dir in $@
+	do
+		count=$((count+1))
+		[ $count -eq 1 ] && continue			
+		mkdir $dir
+		show_message "Directory $dir created in `pwd`"
+	done
+}
+
+function copy_file {
+	dst_dir='.'
+	if [ $# -eq 0 ] || [ $# -gt 2 ]; then
+		show_error "Invalid number of arguments"	
+	elif [ $# -eq 2 ]; then
+		dst_dir=$2
+		if [ $2 = '.' ] || [ $2 = '..' ]; then
+			dst_dir=`readlink -f $dst_dir`
+			echo $dst_dir
+		fi
+	elif [ ! -f $1 ]; then
+		show_error "Source file does not exists"
+	fi
+	cp $1 $dst_dir
+	show_message "File $1 copied to $dst_dir"
 }
--- zend_module.sh
+++ zend_module.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+#title           :zend_module.sh
+#description     :This script will create stucture of zend 3 module.
+#author		 :Sampath K Abeysinghe
+#date            :28-Feb-2019
+#version         :0.1
+#usage		 :bash zend_module.sh <Module Name>
+#notes           :Cannot execute from directory where scipt is located.
+#===================================================================
+
 base_dir=`pwd`
-source $base_dir/base_func.sh
+src_dir=`dirname $0`
+source "$src_dir/base_func.sh"
 
 function show_help {
 	echo -e "Usage:\n zend_module.sh <Module>"
   	exit_func
 }
-
 
 echo " _____                  __   __  ___          __      __    "
 echo "/__  /  ___  ____  ____/ /  /  |/  /___  ____/ /_  __/ /__  "
@@ -23,27 +32,31 @@ echo "Creates Zend 3 Module Structure and creates Controller, Config Files"
 echo "------------------------------------------------------------"
 echo ""
 echo ""
-if [ $# -ne 1 ]
-then
-  echo "Please Provide a name for Module"
-  show_help
-elif [ $1 = "-h" ] || [ $1 = '--help' ]
-then
-  show_help
+
+if [ $src_dir = '.' ]; then
+	show_error "Cannot execute from the directory where script is located"
+elif [ $src_dir = '..' ]; then
+	src_dir=`readlink -f $src_dir`
+fi
+
+if [ $# -ne 1 ]; then
+	echo "Please Provide a name for Module"
+	show_help
+elif [ $1 = "-h" ] || [ $1 = '--help' ]; then
+	show_help
 fi
 
 create_dir $base_dir $1
-#mkdir $1
-#cd $1
-#mkdir config src view
+cd $1
+create_dir `pwd` config src view
 ############## Copy and modify Module.php
-#cd config
-#cp $base_dir/module.config.php .
-#sed -i "<Module>" $1 module.config.php
-#cd ../src/
-#mkdir Controller Form Model
-#cp $base_dir/Module.php .
+cd "$base_dir/$1/config"
+copy_file "$src_dir/module.config.php"
+sed -i "<Module>" $1 module.config.php
+cd ../src/
+create_dir . Controller Form Model
 ############## Copy and modify module.config.php
+#cp $base_dir/Module.php .
 #cd ../view
 #sed -i "<Module>" $1 Module.php
 #dir_name=`echo $1 | tr '[:upper:]' '[:lower:]'`
